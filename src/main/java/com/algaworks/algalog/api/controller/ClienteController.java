d098a4733528b82ad6e07c139fbab3660f6b0347
Carlos Augusto <augustino2003@hotmail.com>
2022-01-18 00:26:18 -0300

    Adicionado flyway, jpa, devtools, classe ClienteRepository

--- src/main/java/com/algaworks/algalog/api/controller/ClienteController.java
+++ src/main/java/com/algaworks/algalog/api/controller/ClienteController.java
@@ -1,31 +1,26 @@
 package com.algaworks.algalog.api.controller;
 
-import java.util.Arrays;
 import java.util.List;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.algaworks.algalog.domain.model.Cliente;
+import com.algaworks.algalog.domain.repository.ClienteRepository;
 
 @RestController
 public class ClienteController {
 	
+	@Autowired
+	private ClienteRepository clienteRepository;
+	
 	@GetMapping("/clientes")
 	public List<Cliente> listar() {
-		Cliente c1 = new Cliente();
-		c1.setId(1L);
-		c1.setNome("Ana");
-		c1.setEmail("ana@gmail.com");
-		c1.setTelefone("36049670");
-		
-		Cliente c2 = new Cliente();
-		c2.setId(2L);
-		c2.setNome("Bob");
-		c2.setEmail("bob@gmail.com");
-		c2.setTelefone("36049693");
-				
-		return Arrays.asList(c1, c2);
+		return clienteRepository.findAll();
 	}
 
 }
